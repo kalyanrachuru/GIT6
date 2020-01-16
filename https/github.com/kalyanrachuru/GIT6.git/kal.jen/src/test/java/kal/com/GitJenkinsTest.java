7f51878ac4f936f0622e51f70148b936b7c4de9b
srikanth kalyan <kalyan.rachuru@gmail.com>
2020-01-16 16:17:11 +0530

    git 6

---
+++ https/github.com/kalyanrachuru/GIT6.git/kal.jen/src/test/java/kal/com/GitJenkinsTest.java
@@ -0,0 +1,12 @@
+package kal.com;
+
+import org.testng.annotations.Test;
+
+public class GitJenkinsTest {
+
+	@Test
+	public void display() {
+		
+		System.out.println("testing git and jenkins ");
+	}
+}
